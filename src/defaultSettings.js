0f45a99790ab5e47bddb0539da9d293262bd6bba
he.wenyao <he.wenyao@outlook.com>
2023-08-29 19:11:32 +0800

    add: layout

---
+++ src/defaultSettings.js
@@ -0,0 +1,10 @@
+export default {
+  "fixSiderbar": true,
+  "layout": "mix",
+  "splitMenus": false,
+  "navTheme": "light",
+  "contentWidth": "Fluid",
+  "colorPrimary": "#1677FF",
+  "fixedHeader": false,
+  "siderMenuType": "sub"
+}
